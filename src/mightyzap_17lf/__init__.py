f04ed09fd0857b9312b431832575f373a37a3df3
Matthew Trentini <matthew.trentini@planetinnovation.com.au>
2026-01-27 09:16:22 +1100

    Expose accelleration, deceleration and other properties

--- src/mightyzap_17lf/__init__.py
+++ src/mightyzap_17lf/__init__.py
@@ -3,14 +3,20 @@
 from pymodbus import FramerType
 from pymodbus.client import ModbusSerialClient
 
-
 class MightyZap17Lf:
     REG_SERIAL_NUMBER = 0
     REG_FIRMWARE_VERSION = 1
+    REG_ACCEL = 15
+    REG_DECEL = 16
+    REG_MIN_POS_OFFSET = 17
+    REG_MAX_POS_OFFSET = 18
+    REG_SPEED_LIMIT = 20
     REG_GOAL_POSITION = 205
     REG_GOAL_SPEED = 208
+    REG_GOAL_CURRENT = 209
     REG_PRESENT_POSITION = 210
     REG_PRESENT_MOTOR_PWM = 213
+
 
     def __init__(self, serial_port: str, baudrate=57_600):
         self.id = 1  # Currently only device #1 is supported
@@ -35,6 +41,33 @@ class MightyZap17Lf:
         return self._read(MightyZap17Lf.REG_FIRMWARE_VERSION)
 
     @property
+    def min_pos_offset(self) -> int:
+        return self._read(MightyZap17Lf.REG_MIN_POS_OFFSET)
+
+    @min_pos_offset.setter
+    def min_pos_offset(self, offset: int):
+        assert 0 <= offset <= 1000
+        self._write(MightyZap17Lf.REG_MIN_POS_OFFSET, offset, device_id=self.id)
+
+    @property
+    def max_pos_offset(self) -> int:
+        return self._read(MightyZap17Lf.REG_MAX_POS_OFFSET)
+
+    @max_pos_offset.setter
+    def max_pos_offset(self, offset: int):
+        assert 0 <= offset <= 1000
+        self._write(MightyZap17Lf.REG_MAX_POS_OFFSET, offset, device_id=self.id)
+
+    @property
+    def speed_limit(self) -> int:
+        return self._read(MightyZap17Lf.REG_SPEED_LIMIT)
+
+    @speed_limit.setter
+    def speed_limit(self, speed: int):
+        assert 0 <= speed <= 1000
+        self._write(MightyZap17Lf.REG_SPEED_LIMIT, speed, device_id=self.id)
+
+    @property
     def position(self) -> int:
         return self._read(MightyZap17Lf.REG_PRESENT_POSITION)
 
@@ -49,8 +82,41 @@ class MightyZap17Lf:
 
     @speed.setter
     def speed(self, speed: int):
-        assert 0 < speed < 1000
+        assert 0 <= speed <= 1000
         self._write(MightyZap17Lf.REG_GOAL_SPEED, speed, device_id=self.id)
+
+    @property
+    def accel(self) -> int:
+        # Note that accel and decel are in units of time so 0 is faster and 1000 is slower
+        return self._read(MightyZap17Lf.REG_ACCEL)
+
+    @accel.setter
+    def accel(self, accel: int):
+        # Note that accel and decel are in units of time so 0 is faster and 1000 is slower
+        assert 0 <= accel <= 1000
+        self._write(MightyZap17Lf.REG_ACCEL, accel, device_id=self.id)
+
+    @property
+    def decel(self) -> int:
+        # Note that accel and decel are in units of time so 0 is faster and 1000 is slower
+        return self._read(MightyZap17Lf.REG_DECEL)
+
+    @decel.setter
+    def decel(self, decel: int):
+        # Note that accel and decel are in units of time so 0 is faster and 1000 is slower
+        assert 0 <= decel <= 1000
+        self._write(MightyZap17Lf.REG_DECEL, decel, device_id=self.id)
+
+    @property
+    def current(self) -> int:
+        # Note that accel and decel are in units of time so 0 is faster and 1000 is slower
+        return self._read(MightyZap17Lf.REG_GOAL_CURRENT)
+
+    @current.setter
+    def current(self, current_mA: int):
+        # Note that accel and decel are in units of time so 0 is faster and 1000 is slower
+        assert 0 <= current_mA <= 1600
+        self._write(MightyZap17Lf.REG_GOAL_CURRENT, current_mA, device_id=self.id)
 
     def _read(self, register: int, device_id: int | None = None) -> int:
         if device_id is None:
